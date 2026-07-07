d71431fee615b05d390bcc0bcbed5f3a69f67ad7
Aisha <Aisha@gmail.com>
2026-07-07 17:29:58 +0400

    Initial clean project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    // Task 2 Function
+    static int Square(int number)
+    {
+        return number * number;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int result = Square(num);
+
+        Console.WriteLine("Square = " + result);
+    }
+}
